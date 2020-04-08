c22e135d86c5fd5d3f195e0ce8e4c9879380cbfa
Orkan Çep <nakro@Orkan-MacBook-Pro.local>
2020-04-08 14:03:22 +0300

    New Updates.

---
+++ SwiftyLoader/SwiftyLoader/SwiftyLoader.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyLoader.h
+//  SwiftyLoader
+//
+//  Created by Orkan Çep on 8.04.2020.
+//  Copyright © 2020 Orkan Çep. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftyLoader.
+FOUNDATION_EXPORT double SwiftyLoaderVersionNumber;
+
+//! Project version string for SwiftyLoader.
+FOUNDATION_EXPORT const unsigned char SwiftyLoaderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyLoader/PublicHeader.h>
+
+
---
+++ SwiftyLoader/SwiftyLoader/SwiftyLoader.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftyLoader.swift
+//  SwiftyLoader
+//
+//  Created by Orkan Çep on 8.04.2020.
+//  Copyright © 2020 Orkan Çep. All rights reserved.
+//
+
+import Foundation
+
+public class SwiftyLoader {
+    
+    private init() {}
+    
+    public static func doSomething() -> String {
+        return "Framework is work."
+    }
+    
+}
